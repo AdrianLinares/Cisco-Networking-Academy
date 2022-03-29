5361d274063e412cc51bbd8f54fa31bebd6201c7
Adrian Linares <jalinaresm@ut.edu.co>
2022-03-28 22:58:43 -0500

    03_personal-info

---
+++ Chapter 1/01_hello-world.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+x=3
+print("This value of X is " + str(x))
+type(x)
+
+x=3
+print("The value of x is " + str(x))
+type(x)
+x=str(x)
+type(x)
+print("The value of x is " + x)
+
+
+num = 22/7
+print(num)
+print("{:.2f}".format(num))
+
+
---
+++ Chapter 1/03_personal-info.py
@@ -0,0 +1,12 @@
+firstName = input("What is your first name? ")
+
+lastName = input("What is your last name? ")
+
+location = input("What is your location? ")
+
+age = input("What is your age? ")
+
+space = " "
+
+print("Hi"+ space + firstName + "!" + space + "Your location is" + space + location + space + "and you are"
+      + space + age + space + "years old.")
